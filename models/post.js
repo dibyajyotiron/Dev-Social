259c8c0cb366cbb2a447e308dbfc2d03b7f3c788
dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2019-05-08 04:29:02 +0530

    changed post schema

--- models/post.js
+++ models/post.js
@@ -1,55 +1,24 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const postSchema = new Schema({
-	user: {
-		type: Schema.Types.ObjectId,
-		ref: "users",
-	},
-	text: {
-		type: String,
-		required: true,
-	},
-	name: {
-		type: String,
-	},
-	avatar: {
-		type: String,
-	},
-	likes: [
-		{
-			user: {
-				type: Schema.Types.ObjectId,
-				ref: "users",
-			},
+const postSchema = new Schema(
+	{
+		user: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
 		},
-	],
-	comments: [
-		{
-			user: {
-				type: Schema.Types.ObjectId,
-				ref: "users",
-			},
-			text: {
-				type: String,
-				required: true,
-			},
-			name: {
-				type: String,
-			},
-			avatar: {
-				type: String,
-			},
-			date: {
-				type: Date,
-				default: Date.now,
-			},
+		text: {
+			type: String,
+			required: true,
 		},
-	],
-	date: {
-		type: Date,
-		default: Date.now,
+		name: {
+			type: String,
+		},
+		avatar: {
+			type: String,
+		},
 	},
-});
+	{ timestamps: true },
+);
 
 module.exports.Post = mongoose.model("Post", postSchema);
