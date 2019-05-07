a343001aaa0398432f8a60e5b094ad59006a0378
dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2019-05-08 04:27:34 +0530

    joi message change

--- middlewares/generic.js
+++ middlewares/generic.js
@@ -4,13 +4,11 @@ module.exports = {
 	validateReqBody(joiSchema) {
 		return (req, res, next) => {
 			const { error } = joiSchema(req.body, req);
-
 			if (error)
 				return res.status(400).json({
 					error: true,
-					message: error.message,
+					message: error.details ? error.details[0].message : error.message,
 				});
-			res.locals.receivedEmail = req.body.email;
 			return next();
 		};
 	},
