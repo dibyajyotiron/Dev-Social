c878a9cc16db7df0fb1a0ab618e2812564149402
dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2019-05-08 04:31:23 +0530

    updated model and added hooks

--- models/profile.js
+++ models/profile.js
@@ -1,120 +1,81 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const profileSchema = new Schema({
-	user: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "user",
-	},
-	company: {
-		type: String,
-		trim: true,
-	},
-	website: {
-		type: String,
-		trim: true,
-	},
-	location: {
-		type: String,
-		trim: true,
-	},
-	status: {
-		type: String,
-		trim: true,
-		required: true,
-	},
-	skills: {
-		type: [String],
-		required: true,
-	},
-	bio: {
-		type: String,
-		trim: true,
-	},
-	githubusername: {
-		type: String,
-		trim: true,
-	},
-	experience: [
-		{
-			title: {
-				type: String,
-				required: true,
-			},
-			company: {
-				type: String,
-				required: true,
-			},
-			location: {
-				type: String,
-			},
-			from: {
-				type: Date,
-				required: true,
-			},
-			to: {
-				type: Date,
-			},
-			current: {
-				type: Boolean,
-				default: false,
-			},
-			description: {
-				type: String,
-			},
+const { educationSchema, experienceSchema } = require("./mixin");
+
+const profileSchema = new Schema(
+	{
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
 		},
-	],
-	education: [
-		{
-			school: {
-				type: String,
-				required: true,
-			},
-			degree: {
-				type: String,
-				required: true,
-			},
-			fieldOfStudy: {
-				type: String,
-				required: true,
-			},
-			from: {
-				type: Date,
-				required: true,
-			},
-			to: {
-				type: Date,
-			},
-			current: {
-				type: Boolean,
-				default: false,
-			},
-			description: {
-				type: String,
-			},
-		},
-	],
-	social: {
-		youtube: {
+		company: {
 			type: String,
+			trim: true,
 		},
-		twitter: {
+		website: {
 			type: String,
+			trim: true,
 		},
-		facebook: {
+		location: {
 			type: String,
+			trim: true,
 		},
-		linkedin: {
+		status: {
 			type: String,
+			trim: true,
+			required: true,
 		},
-		instagram: {
+		skills: {
+			type: [String],
+			required: true,
+		},
+		bio: {
 			type: String,
+			trim: true,
+		},
+		githubUsername: {
+			type: String,
+			trim: true,
+		},
+		experience: [experienceSchema],
+		education: [educationSchema],
+		social: {
+			youtube: {
+				type: String,
+			},
+			twitter: {
+				type: String,
+			},
+			facebook: {
+				type: String,
+			},
+			linkedin: {
+				type: String,
+			},
+			instagram: {
+				type: String,
+			},
 		},
 	},
-	date: {
-		type: Date,
-		default: Date.now,
-	},
+	{ timestamps: true },
+);
+
+profileSchema.pre("find", function(next) {
+	this.populate("user", ["name", "avatar"]);
+	next();
+});
+
+profileSchema.pre("findOne", function(next) {
+	this.populate("user", ["name", "avatar"]);
+	next();
+});
+
+profileSchema.pre("save", function(next, user) {
+	if (this.isNew) {
+		this.user = user._id;
+	}
+	next();
 });
 
 module.exports.Profile = mongoose.model("Profile", profileSchema);
