9d2109a411454749bfcec564f23d8b94deaa8e25
dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2019-05-08 04:30:28 +0530

    added post and put

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,12 +1,28 @@
 const express = require("express"),
 	router = express.Router();
 
-const { getMe } = require("../controllers/profileController");
+const { getMe, createOrUpdate } = require("../controllers/profileController");
 const { auth, getLoggedInUser } = require("../middlewares/userMiddleware");
-// @route GET api/profile/test
-// @desc Tests PROFILE route
-// @access Public
+const { validateProfileSchema } = require("../models/joiSchema");
+
+const { validateReqBody } = require("../middlewares/generic");
+
+// @route GET /profile/me
+// @desc Get Logged in user PROFILE route
+// @access Private
 
 router.get("/me", auth, getLoggedInUser, getMe);
+
+// @route POST /profile/
+// @desc Create USER PROFILE route
+// @access Private
+
+router.post("/", auth, getLoggedInUser, validateReqBody(validateProfileSchema), createOrUpdate);
+
+// @route PUT /profile/
+// @desc Update USER PROFILE route
+// @access Private
+
+router.put("/", auth, getLoggedInUser, validateReqBody(validateProfileSchema), createOrUpdate);
 
 module.exports = router;
