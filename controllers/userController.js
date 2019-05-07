4395a5af29e2c3808182bbab0aa97874a591ee75
dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2019-05-08 04:27:19 +0530

    minor modification

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,16 +8,15 @@ module.exports = {
 		const { url } = req;
 		let { name, email, password } = req.body;
 		let user = res.locals.user;
+
 		switch (true) {
 			case user && url.split("/").includes("login"):
 				const validPassword = await bcrypt.compare(password, user.password);
 				if (!validPassword) return res.status(400).send("Invalid password!");
 				const token = user.generateAuthToken();
 				return res.header("x-auth-token", token).json({ success: true, message: "Logged in successfully logged in!" });
-
 			case !user && url.split("/").includes("login"):
 				return res.status(404).json({ error: true, message: "User is not registered!" });
-
 			case user && url.split("/").includes("register"):
 				return res.status(400).json({ error: true, message: "User with this email id is already registered!" });
 		}
@@ -28,7 +27,7 @@ module.exports = {
 
 		await user.save(req.body);
 		const token = user.generateAuthToken();
-
-		return res.header("x-auth-token", token).send(({ name, email, avatar } = user));
+		({ name, email, avatar } = user);
+		return res.header("x-auth-token", token).json({ name, email, avatar });
 	},
 };
