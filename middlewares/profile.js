a8b302ee24e2e851f13d72e3621c34e86d509b6f
dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2019-05-09 04:24:43 +0530

    added profile middleware

---
+++ middlewares/profile.js
@@ -0,0 +1,21 @@
+const { Profile } = require("../models/profile");
+
+module.exports = {
+	async getLoggedInUserProfile(req, res, next) {
+		const userProfile = await Profile.findOne({ user: req.user._id });
+		res.locals.me = userProfile;
+		return next();
+	},
+	async getAllUsers(req, res, next) {
+		const usersProfile = await Profile.find({});
+		res.locals.profiles = usersProfile;
+		return next();
+	},
+	async findUserById(req, res, next) {
+		const { user_id: userId } = req.params;
+		const user = await Profile.findOne({ user: userId });
+		res.locals.user = user;
+		if (!user) return res.status(404).json({ error: true, message: "User with given id doesn't exist!" });
+		return next();
+	},
+};
