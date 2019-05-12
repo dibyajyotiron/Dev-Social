f7f9917fdfe2084a97294ce1fd7a8c2b2d90b7aa
dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2019-05-13 01:41:00 +0530

    added controller helper function

---
+++ controllers/helpers/post.js
@@ -0,0 +1,30 @@
+const { PostReaction } = require("../../models/postReact");
+const { Post } = require("../../models/post");
+
+module.exports = {
+	async getPostWithReactions(post_id = null) {
+		let result;
+
+		if (post_id) {
+			const post = await Post.findById(post_id)
+				.sort({ createdAt: -1 })
+				.lean();
+			const reactions = await PostReaction.find({ _post: post_id }).lean();
+
+			result = { ...post, reactions };
+		} else {
+			const posts = await Post.find({})
+				.sort({ createdAt: -1 })
+				.lean();
+			const postIds = posts.map(post => post._id);
+			const reactions = await PostReaction.find({ _post: { $in: postIds } }).lean();
+
+			for (let post of posts) {
+				post.reactions = reactions.filter(r => r._post.equals(post._id));
+			}
+			result = posts;
+		}
+
+		return result;
+	},
+};
