b99088f9c388edcdaebb57db6fe0902bde7d5e7c
dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2020-08-24 23:03:30 +0530

    follow user

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,32 +2,63 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 
 const { User } = require("../models/user");
+const { Follow } = require("../models/follow");
 
 module.exports = {
-	async loginOrRegisterUser(req, res) {
-		const { url } = req;
-		let { name, email, password } = req.body;
-		let user = res.locals.user;
+  async loginOrRegisterUser(req, res) {
+    const { url } = req;
+    let { name, email, password } = req.body;
+    let user = res.locals.user;
 
-		switch (true) {
-			case user && url.split("/").includes("login"):
-				const validPassword = await bcrypt.compare(password, user.password);
-				if (!validPassword) return res.status(400).send("Invalid password!");
-				const token = user.generateAuthToken();
-				return res.header("x-auth-token", token).json({ success: true, message: "User successfully logged in!" });
-			case !user && url.split("/").includes("login"):
-				return res.status(404).json({ error: true, message: "User is not registered!" });
-			case user && url.split("/").includes("register"):
-				return res.status(400).json({ error: true, message: "User with this email id is already registered!" });
-		}
+    switch (true) {
+      case user && url.split("/").includes("login"):
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) return res.status(400).send("Invalid password!");
+        const token = user.generateAuthToken();
+        return res
+          .header("x-auth-token", token)
+          .json({ success: true, message: "User successfully logged in!" });
+      case !user && url.split("/").includes("login"):
+        return res
+          .status(404)
+          .json({ error: true, message: "User is not registered!" });
+      case user && url.split("/").includes("register"):
+        return res.status(400).json({
+          error: true,
+          message: "User with this email id is already registered!",
+        });
+    }
 
-		let avatar = gravatar.url(email, { s: "200", r: "pg", d: "mm" });
+    let avatar = gravatar.url(email, { s: "200", r: "pg", d: "mm" });
 
-		user = new User({ name, email, avatar, password });
+    user = new User({ name, email, avatar, password });
 
-		await user.save(req.body);
-		const token = user.generateAuthToken();
-		({ name, email, avatar } = user);
-		return res.header("x-auth-token", token).json({ name, email, avatar });
-	},
+    await user.save(req.body);
+    const token = user.generateAuthToken();
+    ({ name, email, avatar } = user);
+    return res.header("x-auth-token", token).json({ name, email, avatar });
+  },
+  async followUser(req, res) {
+    const { _follows } = req.body;
+    const { user } = res.locals;
+    const alreadyFollows = await Follow.findOne({
+      _user: user._id,
+      _follows,
+    });
+    if (!alreadyFollows) {
+      await Follow.create({
+        _user: user.id,
+        _follows,
+      });
+      return res.json({
+        success: true,
+        message: "Followed successfully!",
+      });
+    }
+    await alreadyFollows.remove();
+    return res.json({
+      success: true,
+      message: "Unfollowed successfully!",
+    });
+  },
 };
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const router = express.Router();
 const { validateReqBody } = require("../middlewares/generic");
-const { validateUser } = require("../middlewares/userMiddleware");
+const { validateUser, auth } = require("../middlewares/userMiddleware");
 const { validateUserSchema } = require("../models/joiSchema");
-const { loginOrRegisterUser } = require("../controllers/userController");
+const {
+  loginOrRegisterUser,
+  followUser,
+} = require("../controllers/userController");
 
 // @route    POST api/users
 // @desc     Register user
 // @access   Public
-router.post("/register", validateReqBody(validateUserSchema), validateUser, loginOrRegisterUser);
-router.post("/login", validateReqBody(validateUserSchema), validateUser, loginOrRegisterUser);
+router.post(
+  "/register",
+  validateReqBody(validateUserSchema),
+  validateUser,
+  loginOrRegisterUser
+);
+router.post(
+  "/login",
+  validateReqBody(validateUserSchema),
+  validateUser,
+  loginOrRegisterUser
+);
+router.post(
+  "/follow",
+  validateReqBody(validateUserFollowSchema),
+  auth,
+  followUser
+);
 module.exports = router;
