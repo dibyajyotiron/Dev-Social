6266f9d0414c98581e55a24617c1cd86780a3972
dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2020-08-25 00:24:13 +0530

    unified topology

--- services/db.js
+++ services/db.js
@@ -3,15 +3,21 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 module.exports = () => {
-	mongoose
-		.connect(config.get("mongoURI"), {
-			useNewUrlParser: true,
-			useCreateIndex: true,
-			useFindAndModify: false,
-		})
-		.then(() => {
-			if (process.env.NODE_ENV !== "production") return logger.info("Connected database: " + `${config.get("mongoURI")}...`.green);
-			return logger.info("connected to production environment of mongodb...".blue);
-		})
-		.catch(ex => logger.error(`${ex.message}`));
+  mongoose
+    .connect(config.get("mongoURI"), {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      if (process.env.NODE_ENV !== "production")
+        return logger.info(
+          "Connected database: " + `${config.get("mongoURI")}...`.green
+        );
+      return logger.info(
+        "connected to production environment of mongodb...".blue
+      );
+    })
+    .catch((ex) => logger.error(`${ex.message}`));
 };
