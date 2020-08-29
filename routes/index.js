d60a586cd35389e82664321561c895212f71fa5b
dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2020-08-29 22:49:32 +0530

    routes added

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,15 @@ const post = require("./posts");
 const users = require("./users");
 const profiles = require("./profiles");
 
-const { internalServerError, notFoundError } = require("../middlewares/error");
+const { internalServerError, notFoundError, genericError } = require("../middlewares/error");
 
-module.exports = app => {
+module.exports = (app) => {
 	app.use(express.json());
 	app.use("/posts", post);
 	app.use("/users", users);
 	app.use("/profiles", profiles);
+	app.use(genericError);
 	app.use(notFoundError);
+	// most likely it shouldn't reach internalServerError as internal server error is already in generic
 	app.use(internalServerError);
 };
