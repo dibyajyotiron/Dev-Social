e93ee95394396cbfc4050b3e9e785edffd1700b3
dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2020-08-29 22:48:03 +0530

    formatting and schema for follow

--- models/joiSchema.js
+++ models/joiSchema.js
@@ -4,113 +4,101 @@ Joi.objectId = require("joi-objectid")(Joi);
 const validUsernameSchema = () => Joi.string().max(50);
 
 const getValidCareerSchema = (type, req) => {
-  let validSchema;
+	let validSchema;
 
-  const commonProperties = {
-    from: req.method === "POST" ? Joi.date().required() : Joi.date(),
-    to: Joi.date(),
-    current: Joi.bool(),
-    description: Joi.string().max(150),
-  };
+	const commonProperties = {
+		from: req.method === "POST" ? Joi.date().required() : Joi.date(),
+		to: Joi.date(),
+		current: Joi.bool(),
+		description: Joi.string().max(150),
+	};
 
-  switch (type) {
-    case "education":
-      validSchema = {
-        ...commonProperties,
-        school: req.method === "POST" ? Joi.string().required() : Joi.string(),
-        degree: req.method === "POST" ? Joi.string().required() : Joi.string(),
-        fieldOfStudy:
-          req.method === "POST" ? Joi.string().required() : Joi.string(),
-      };
-      break;
+	switch (type) {
+		case "education":
+			validSchema = {
+				...commonProperties,
+				school: req.method === "POST" ? Joi.string().required() : Joi.string(),
+				degree: req.method === "POST" ? Joi.string().required() : Joi.string(),
+				fieldOfStudy: req.method === "POST" ? Joi.string().required() : Joi.string(),
+			};
+			break;
 
-    case "experience":
-      validSchema = {
-        ...commonProperties,
-        title: req.method === "POST" ? Joi.string().required() : Joi.string(),
-        company: req.method === "POST" ? Joi.string().required() : Joi.string(),
-        location: Joi.string(),
-      };
-      break;
+		case "experience":
+			validSchema = {
+				...commonProperties,
+				title: req.method === "POST" ? Joi.string().required() : Joi.string(),
+				company: req.method === "POST" ? Joi.string().required() : Joi.string(),
+				location: Joi.string(),
+			};
+			break;
 
-    default:
-      validSchema = { error: true, message: "Wrong career type provided!" };
-  }
-  return validSchema;
+		default:
+			validSchema = { error: true, message: "Wrong career type provided!" };
+	}
+	return validSchema;
 };
 
-const validCareerSchema = (type, req, maxAllowed) =>
-  Joi.object().keys(getValidCareerSchema(type, req)).max(maxAllowed);
+const validCareerSchema = (type, req, maxAllowed) => Joi.object().keys(getValidCareerSchema(type, req)).max(maxAllowed);
 
 module.exports = {
-  validateUserSchema(user, req) {
-    const schema = {
-      name: req.url.split("/").includes("register")
-        ? Joi.string()
-            .min(2)
-            .max(50)
-            .required()
-            .error(
-              new Error(
-                '"Name" of at least 2 and at max 50 letters is required!'
-              )
-            )
-        : Joi.forbidden().error(new Error("Name is not allowed!")),
-      email: Joi.string().email().required(),
-      password: Joi.string()
-        .regex(/^[a-zA-Z0-9]{8,16}$/)
-        .min(8)
-        .max(16)
-        .required(),
-      avatar: Joi.string().uri(),
-    };
-    return Joi.validate(user, schema);
-  },
-  validateProfileSchema(profile, req) {
-    const schema = {
-      company: Joi.string().max(50),
-      website: Joi.string().max(75),
-      location: Joi.string().max(100),
-      status: req.method === "POST" ? Joi.string().required() : Joi.string(),
-      skills: req.method === "POST" ? Joi.string().required() : Joi.string(),
-      bio: Joi.string().max(150),
-      githubUsername: validUsernameSchema(),
+	validateUserSchema(user, req) {
+		const schema = {
+			name: req.url.split("/").includes("register") ? Joi.string().min(2).max(50).required().error(new Error('"Name" of at least 2 and at max 50 letters is required!')) : Joi.forbidden().error(new Error("Name is not allowed!")),
+			email: Joi.string().email().required(),
+			password: Joi.string()
+				.regex(/^[a-zA-Z0-9]{8,16}$/)
+				.min(8)
+				.max(16)
+				.required(),
+			avatar: Joi.string().uri(),
+		};
+		return Joi.validate(user, schema);
+	},
+	validateProfileSchema(profile, req) {
+		const schema = {
+			company: Joi.string().max(50),
+			website: Joi.string().max(75),
+			location: Joi.string().max(100),
+			status: req.method === "POST" ? Joi.string().required() : Joi.string(),
+			skills: req.method === "POST" ? Joi.string().required() : Joi.string(),
+			bio: Joi.string().max(150),
+			githubUsername: validUsernameSchema(),
 
-      social: Joi.object().keys({
-        youtube: validUsernameSchema(),
-        twitter: validUsernameSchema(),
-        facebook: validUsernameSchema(),
-        linkedin: validUsernameSchema(),
-        instagram: validUsernameSchema(),
-      }),
-    };
-    return Joi.validate(profile, schema);
-  },
-  validateCareerSchema(career, req) {
-    let validSchema;
-    let { url } = req;
-    url = url.split("/");
-    switch (true) {
-      case url.includes("experience"):
-        validSchema = validCareerSchema("experience", req, 10);
-        break;
-      case url.includes("education"):
-        validSchema = validCareerSchema("education", req, 10);
-        break;
-    }
-    return Joi.validate(career, validSchema);
-  },
-  validateComment(comment) {
-    const schema = {
-      text: Joi.string().min(2).max(150),
-      name: Joi.string().min(2).max(25),
-    };
-    return Joi.validate(comment, schema);
-  },
-  validateUserFollowSchema(follow) {
-    const schema = {
-      _follows: Joi.objectId(),
-    };
-    return Joi.validate(follow, schema);
-  },
+			social: Joi.object().keys({
+				youtube: validUsernameSchema(),
+				twitter: validUsernameSchema(),
+				facebook: validUsernameSchema(),
+				linkedin: validUsernameSchema(),
+				instagram: validUsernameSchema(),
+			}),
+		};
+		return Joi.validate(profile, schema);
+	},
+	validateCareerSchema(career, req) {
+		let validSchema;
+		let { url } = req;
+		url = url.split("/");
+		switch (true) {
+			case url.includes("experience"):
+				validSchema = validCareerSchema("experience", req, 10);
+				break;
+			case url.includes("education"):
+				validSchema = validCareerSchema("education", req, 10);
+				break;
+		}
+		return Joi.validate(career, validSchema);
+	},
+	validateComment(comment) {
+		const schema = {
+			text: Joi.string().min(2).max(150),
+			name: Joi.string().min(2).max(25),
+		};
+		return Joi.validate(comment, schema);
+	},
+	validateUserFollowSchema(follow) {
+		const schema = {
+			_follows: Joi.objectId().required(),
+		};
+		return Joi.validate(follow, schema);
+	},
 };
